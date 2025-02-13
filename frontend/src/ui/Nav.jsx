90f37b2af182693d28d704f1f73d749bcaa4d086
NSCCDylanC <144455927+NSCCDylanC@users.noreply.github.com>
2025-02-13 14:50:20 -0400

    routes created, home-login

---
+++ frontend/src/ui/Nav.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Nav() {
+    return (
+        <>
+        <nav style={{display: 'flex', justifyContent: 'space-around', padding: '10px', backgroundColor: '#101112', borderBottom: '2px solid #e94560' }}>
+
+            <Link to="/" className="btn btn-primary" backgroundColor="#004780" style={{ margin: '0 10px '}}>Home</Link>
+
+            <Link to="/login" className="btn btn-primary" style={{ margin: '0 10px' }}>Login</Link>
+        </nav>
+        </>
+);
+
+}
