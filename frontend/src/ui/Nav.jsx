130a3f8161a5914c8b0a6a0eac2d2ba9258b4bed
NSCCDylanC <144455927+NSCCDylanC@users.noreply.github.com>
2025-02-13 15:13:14 -0400

    login form added

--- frontend/src/ui/Nav.jsx
+++ frontend/src/ui/Nav.jsx
@@ -6,9 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Nav() {
     return (
         <>
-        <nav style={{display: 'flex', justifyContent: 'space-around', padding: '10px', backgroundColor: '#101112', borderBottom: '2px solid #e94560' }}>
+        <nav style={{display: 'flex', padding: '10px', width: '100%', backgroundColor: '#101112', borderBottom: '2px solid #e94560' }}>
 
             <Link to="/" className="btn btn-primary" backgroundColor="#004780" style={{ margin: '0 10px '}}>Home</Link>
+            
 
             <Link to="/login" className="btn btn-primary" style={{ margin: '0 10px' }}>Login</Link>
         </nav>
