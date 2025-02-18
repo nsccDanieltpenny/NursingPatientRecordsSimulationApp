62903b244441946cb2795df463995e7d9192aeb6
Kangjie Su <W0481026@campus.nscc.ca>
2025-02-18 17:18:18 -0400

    update cors

--- NursingEducationalBackend/NursingEducationalBackend/Program.cs
+++ NursingEducationalBackend/NursingEducationalBackend/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,10 +25,16 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
+
+
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "Hello from Nursing Educational Backend!");
 
 app.Run();
