2d2cfd6f23afa756502c57bd8f9a85ec10343de6
Mitchell Yetman <w0498497@nscc.ca>
2025-03-18 15:10:32 -0300

    removed tests

--- NursingEducationalBackend/NursingEducationalBackend/obj/Debug/net9.0/NursingEducationalBackend.AssemblyInfo.cs
+++ NursingEducationalBackend/NursingEducationalBackend/obj/Debug/net9.0/NursingEducationalBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3640f30ae7df942c4d9f42222262b0280d068e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc187139dd621f99c1d5093af7da1026c4c21791")]
 [assembly: System.Reflection.AssemblyProductAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
