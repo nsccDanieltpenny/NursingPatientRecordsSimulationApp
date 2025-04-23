213a89d6c5cfddaba9797343cf8cfe1184f5db49
Mitchell Yetman <w0498497@nscc.ca>
2025-04-23 12:48:22 -0300

    final commit before re-working version control. cleared and re-added database to reflect model changes. everything seems to work.

--- NursingEducationalBackend/NursingEducationalBackend/obj/Debug/net9.0/NursingEducationalBackend.AssemblyInfo.cs
+++ NursingEducationalBackend/NursingEducationalBackend/obj/Debug/net9.0/NursingEducationalBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493947443d3684d6bf814f2b2c1f0e511fed30d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ba8a8364962af45568bf33944a129642b4cf72")]
 [assembly: System.Reflection.AssemblyProductAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NursingEducationalBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
