1206a1fb8a133de9eb70ae5c52d2de0280b158a9
NSCCDylan <W0443022@nscc.ca>
2025-04-25 15:54:03 -0300

    works in local

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -7,23 +7,25 @@ const UserContext = createContext();
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
-  
+  const [cookies, setCookie] = useCookies(['nurse']);
   const APIHOST = import.meta.env.VITE_API_URL;
 
   const login = async (credentials) => {
     setLoading(true);
     
     try {
-      const response = await axios.post(`${APIHOST}/api/Auth/login`, {
-        Email: credentials.email,
-        Password: credentials.password
+      const response = await axios.post(`${APIHOST}/api/auth/login`, {
+        Email: credentials.Email,
+        Password: credentials.Password
       });
 
+      setCookie('nurse', response.data, { path: '/' });
       setUser(response.data);
       return true;
       
     } catch (error) {
       console.error('Login error:', error.response?.data || error.message);
+      
       return false;
     } finally {
       setLoading(false);
@@ -32,16 +34,11 @@ export function UserProvider({ children }) {
 
   const logout = () => {
     setUser(null);
-    
+    setCookie('nurse', '', { path: '/' });
   };
 
   return (
-    <UserContext.Provider value={{
-       user, 
-       login, 
-       logout, 
-       loading 
-    }}>
+    <UserContext.Provider value={{ user, login, logout, loading }}>
       {children}
     </UserContext.Provider>
   );
