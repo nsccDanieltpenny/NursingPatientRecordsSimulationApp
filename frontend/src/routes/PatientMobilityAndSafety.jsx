1675d13048a682402c508e85fa958fea597a5688
Mitchell Yetman <w0498497@nscc.ca>
2025-05-14 14:43:02 -0300

    another tiny change to mobility

--- frontend/src/routes/PatientMobilityAndSafety.jsx
+++ frontend/src/routes/PatientMobilityAndSafety.jsx
@@ -184,9 +184,13 @@ const PatientMobilityAndSafety = () => {
             if (profileData) {
                 const filteredProfileData = Object.fromEntries(Object.entries(profileData).filter(([_, value]) => value != null && value !== ''));
                 if (Object.keys(filteredProfileData).length > 0) {
-                    if (filteredProfileData.isolationPrecautions == "No" && filteredProfileData.isolationPrecautionsTimestamp || filteredProfileData.isolationPrecautionDetails) {
-                        delete filteredProfileData.isolationPrecautionsTimestamp;
-                        delete filteredProfileData.isolationPrecautionDetails;
+                    if (filteredProfileData.isolationPrecautions == "No") {
+                        if (filteredProfileData.isolationPrecautionsTimestamp) {
+                            delete filteredProfileData.isolationPrecautionsTimestamp;
+                        }
+                        if (filteredProfileData.isolationPrecautionDetails) {
+                            delete filteredProfileData.isolationPrecautionDetails;
+                        }
                         localStorage.setItem(`patient-profile-${id}`, JSON.stringify(filteredProfileData));
                     } else {
                         localStorage.setItem(`patient-profile-${id}`, JSON.stringify(filteredProfileData));
