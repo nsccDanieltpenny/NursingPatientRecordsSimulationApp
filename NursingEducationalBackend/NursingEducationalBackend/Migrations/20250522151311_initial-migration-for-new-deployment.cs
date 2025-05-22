e126ec4e77faa64e365446361259b5c1f7e2d92a
Mitchell Yetman <w0498497@nscc.ca>
2025-05-22 13:51:44 -0300

    hooked up to azure successfully

---
+++ NursingEducationalBackend/NursingEducationalBackend/Migrations/20250522151311_initial-migration-for-new-deployment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NursingEducationalBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigrationfornewdeployment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pronounce",
+                table: "Patient",
+                newName: "Pronouns");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pronouns",
+                table: "Patient",
+                newName: "Pronounce");
+        }
+    }
+}
