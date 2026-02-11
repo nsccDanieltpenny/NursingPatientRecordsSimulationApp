74f0ceb79d19e2124f4b64da76191a4d723a4905
Cody F. <W0400746@nscc.ca>
2026-02-11 18:54:50 -0400

    Add migration for acute rotation

---
+++ NursingEducationalBackend/NursingEducationalBackend/Migrations/20260211225347_AddAcuteRotation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NursingEducationalBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcuteRotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Rotations",
+                columns: new[] { "RotationId", "Name" },
+                values: new object[] { 2, "Acute Care" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Rotations",
+                keyColumn: "RotationId",
+                keyValue: 2);
+        }
+    }
+}
