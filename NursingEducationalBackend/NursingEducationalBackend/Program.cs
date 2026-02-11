09bf22a852a9835234f7238b40b626b8dee3f15b
thefrostycoder <22138347+thefrostycoder@users.noreply.github.com>
2026-02-11 15:00:29 -0400

    Add database retrying to backend
    Added retry parameters to the database setup in the API, this should hopefully put an end to the issues when someone tries to interact with the app after the servers pause

--- NursingEducationalBackend/NursingEducationalBackend/Program.cs
+++ NursingEducationalBackend/NursingEducationalBackend/Program.cs
@@ -32,7 +32,12 @@ var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnec
 
 
 builder.Services.AddDbContext<NursingDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure(
+        maxRetryCount: 5,
+        maxRetryDelay: TimeSpan.FromSeconds(30),
+        errorNumbersToAdd: null
+    )));
 
 // Add Identity services
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
@@ -184,4 +189,5 @@ if (app.Environment.IsDevelopment())
         }
     }
 }
+
     app.Run();
