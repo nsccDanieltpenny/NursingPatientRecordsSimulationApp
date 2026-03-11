924ffc2a330a3cd0923978e271d93b4823353af9
Aj Howell <Ajhowelll@hotmail.com>
2026-03-11 11:56:36 -0300

    Add Missing Migrations
    Possible fix to azure 500.3 erro

---
+++ NursingEducationalBackend/NursingEducationalBackend/Migrations/20260311043917_FixPendingModelChanges.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NursingEducationalBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CampusId",
+                table: "Class",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Campuses",
+                columns: table => new
+                {
+                    CampusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campuses", x => x.CampusId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Class_CampusId",
+                table: "Class",
+                column: "CampusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Class_Campuses_CampusId",
+                table: "Class",
+                column: "CampusId",
+                principalTable: "Campuses",
+                principalColumn: "CampusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Class_Campuses_CampusId",
+                table: "Class");
+
+            migrationBuilder.DropTable(
+                name: "Campuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Class_CampusId",
+                table: "Class");
+
+            migrationBuilder.DropColumn(
+                name: "CampusId",
+                table: "Class");
+        }
+    }
+}
