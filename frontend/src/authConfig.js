7ac612494e1a533ccc966f0e74982bc396804e2b
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-05-22 13:43:41 -0300

    Format with prettier

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -1,15 +1,15 @@
 export const msalConfig = {
-    auth: {
-        clientId: import.meta.env.VITE_AD_CLIENTID,
-        authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AD_TENANTID}`,
-        redirectUri: import.meta.env.VITE_AD_REDIRECTURI,
-    },
-    cache: {
-        cacheLocation: "sessionStorage",
-        storeAuthStateInCookie: false,
-    }
+  auth: {
+    clientId: import.meta.env.VITE_AD_CLIENTID,
+    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AD_TENANTID}`,
+    redirectUri: import.meta.env.VITE_AD_REDIRECTURI,
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false,
+  },
 };
 
 export const loginRequest = {
-    scopes: [`api://${import.meta.env.VITE_AD_APICLIENTID}/access_as_user`]
+  scopes: [`api://${import.meta.env.VITE_AD_APICLIENTID}/access_as_user`],
 };
